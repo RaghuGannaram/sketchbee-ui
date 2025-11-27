87f12f67014282c4a3ecde04f6b8c46829052a50
Raghu Gannaram <raghuvarangannaram@gmail.com>
2025-11-27 21:21:44 +0530

    Updated useSocket hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,30 @@
-import { GameProvider } from "./contexts/GameContext";
+import { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { GameProvider } from "./contexts/GameContext";
 import Home from "./pages/Home";
 import Game from "./pages/Game";
 import useLocalStorage from "./hooks/useLocalStorage";
+import useSocket from "./hooks/useSocket";
 import "./App.css";
 
 export default function App() {
-    const [handle, _setHandle] = useLocalStorage<string>("sketchbee:handle", "");
+    const [userHandle, _setUserHandle] = useLocalStorage<string>("sketchbee:handle", "");
+    const { emit } = useSocket();
 
-    console.log("sketchbee-log: user handle, ", handle);
+    useEffect(() => {
+        emit("greet", { greeting: "Hello server..!" }, (response: any) => {
+            console.log(`sketchbee-log: server greeted at ${response.serverTime} :`, response.greeting);
+        });
+    }, []);
+
+    console.log("sketchbee-log: user handle, ", userHandle);
 
     return (
         <GameProvider>
             <Router>
                 <Routes>
                     <Route path="/" element={<Home />} />
-                    <Route path="/game" element={handle ? <Game /> : <Navigate to="/" replace />} />
+                    <Route path="/game" element={userHandle ? <Game /> : <Navigate to="/" replace />} />
                 </Routes>
             </Router>
         </GameProvider>
---
+++ src/hooks/useSocket.ts
@@ -0,0 +1,70 @@
+import { useEffect, useCallback } from "react";
+import { io, type Socket } from "socket.io-client";
+
+const socket: Socket = io(import.meta.env.VITE_SOCKET_URL, {
+    autoConnect: false,
+});
+
+console.info("sketchbee-info: socket connection triggered : ", socket.io.engine?.transport.name);
+
+function useSocket() {
+    useEffect(() => {
+        const handleConnect = () => {
+            console.info("sketchbee-info: socket connected : ", socket.id);
+        };
+
+        const handleDisconnect = () => {
+            console.info("sketchbee-info: socket disconnected");
+        };
+
+        if (!socket.connected) {
+            socket.connect();
+        }
+
+        socket.on("connect", handleConnect);
+
+        socket.on("disconnect", handleDisconnect);
+
+        return () => {
+            socket.off("connect", handleConnect);
+            socket.off("disconnect", handleDisconnect);
+        };
+    }, []);
+
+    const emit = useCallback(
+        (eventName: string, data: any, ackCallback?: (response: any) => void): Promise<any> | void => {
+            // if (!socket.connected) {
+            //     console.warn("sketchbee-warn: socket not connected, cannot emit event:", eventName);
+            //     return;
+            // }
+
+            if (ackCallback && typeof ackCallback === "function") {
+                console.debug(`sketchbee-debug: (Ack Request)-${eventName} ==>`, data);
+
+                socket.emit(eventName, data, (response: any) => {
+                    console.debug(`sketchbee-debug: (Ack Response)-${eventName} <==`, response);
+                    ackCallback(response);
+                });
+            } else {
+                console.debug(`sketchbee-debug: (Fire & Forget)-${eventName} ==>`, data);
+                socket.emit(eventName, data);
+            }
+        },
+        []
+    );
+
+    const subscribe = useCallback((eventName: string, callback: (data: any) => void) => {
+        const handler = (data: any) => {
+            console.debug(`sketchbee-debug: (Event Received)-${eventName} <==`, data);
+            callback(data);
+        };
+
+        socket.on(eventName, handler);
+
+        return () => socket.off(eventName, handler);
+    }, []);
+
+    return { socket, emit, subscribe };
+}
+
+export default useSocket;
